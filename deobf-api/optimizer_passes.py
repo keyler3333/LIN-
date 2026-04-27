6d479a895d977caa0000e1fcf06da22ab2ae1d5a
keyler3333 <kyturk42@gmail.com>
2026-04-27 19:36:48 -0400

    Update optimizer_passes.py

--- deobf-api/optimizer_passes.py
+++ deobf-api/optimizer_passes.py
@@ -1,119 +1,133 @@
-from ir_builder import Block, If, While, Repeat, ForNumeric, ForGeneric, FunctionDef, LocalDecl, Assignment, Number, String, Boolean, Nil, BinOp, UnaryOp, Name, Return, Break, Vararg
+import copy
+from ir_builder import (Block, If, While, Repeat, ForNumeric, ForGeneric,
+                         FunctionDef, LocalDecl, Assignment, Number, String,
+                         Boolean, Nil, BinOp, UnaryOp, Name, Return, Break,
+                         Vararg, FunctionCall, Index, TableConstructor)
 
-def remove_dead_code(ir_node):
-    if isinstance(ir_node, Block):
+def _collect_used(node, used):
+    if node is None:
+        return
+    if isinstance(node, Name):
+        used.add(node.name)
+        return
+    if isinstance(node, (Number, String, Boolean, Nil, Vararg, Break)):
+        return
+    if isinstance(node, BinOp):
+        _collect_used(node.left, used)
+        _collect_used(node.right, used)
+    elif isinstance(node, UnaryOp):
+        _collect_used(node.operand, used)
+    elif isinstance(node, FunctionCall):
+        _collect_used(node.func, used)
+        for a in node.args:
+            _collect_used(a, used)
+    elif isinstance(node, Index):
+        _collect_used(node.table, used)
+        _collect_used(node.key,   used)
+    elif isinstance(node, TableConstructor):
+        for k, v in node.fields:
+            if k: _collect_used(k, used)
+            _collect_used(v, used)
+    elif isinstance(node, Return):
+        for v in node.values:
+            _collect_used(v, used)
+    elif isinstance(node, Assignment):
+        for t in node.targets: _collect_used(t, used)
+        for v in node.values:  _collect_used(v, used)
+    elif isinstance(node, LocalDecl):
+        for v in node.values: _collect_used(v, used)
+    elif isinstance(node, Block):
+        for s in node.statements: _collect_used(s, used)
+    elif isinstance(node, If):
+        _collect_used(node.test, used)
+        _collect_used(node.body, used)
+        if node.orelse: _collect_used(node.orelse, used)
+    elif isinstance(node, While):
+        _collect_used(node.test, used)
+        _collect_used(node.body, used)
+    elif isinstance(node, Repeat):
+        _collect_used(node.body, used)
+        _collect_used(node.test, used)
+    elif isinstance(node, ForNumeric):
+        _collect_used(node.start, used)
+        _collect_used(node.end,   used)
+        if node.step: _collect_used(node.step, used)
+        _collect_used(node.body, used)
+    elif isinstance(node, ForGeneric):
+        for i in node.iterators: _collect_used(i, used)
+        _collect_used(node.body, used)
+    elif isinstance(node, FunctionDef):
+        _collect_used(node.body, used)
+
+def remove_dead_code(node):
+    if isinstance(node, Block):
         new_stmts = []
-        for stmt in ir_node.statements:
+        for stmt in node.statements:
             cleaned = remove_dead_code(stmt)
             if cleaned is not None:
                 new_stmts.append(cleaned)
         return Block(new_stmts)
-    if isinstance(ir_node, If):
-        if isinstance(ir_node.test, Boolean):
-            if ir_node.test.value is True:
-                return remove_dead_code(ir_node.body)
-            elif ir_node.test.value is False:
-                if ir_node.orelse:
-                    return remove_dead_code(ir_node.orelse)
-                return None
-        ir_node.test = remove_dead_code(ir_node.test)
-        ir_node.body = remove_dead_code(ir_node.body)
-        if ir_node.orelse:
-            ir_node.orelse = remove_dead_code(ir_node.orelse)
-        return ir_node
-    if isinstance(ir_node, While):
-        if isinstance(ir_node.test, Boolean) and ir_node.test.value is False:
-            return None
-        ir_node.test = remove_dead_code(ir_node.test)
-        ir_node.body = remove_dead_code(ir_node.body)
-        return ir_node
-    if isinstance(ir_node, Repeat):
-        ir_node.body = remove_dead_code(ir_node.body)
-        ir_node.test = remove_dead_code(ir_node.test)
-        return ir_node
-    if isinstance(ir_node, ForNumeric):
-        ir_node.start = remove_dead_code(ir_node.start)
-        ir_node.end = remove_dead_code(ir_node.end)
-        if ir_node.step:
-            ir_node.step = remove_dead_code(ir_node.step)
-        ir_node.body = remove_dead_code(ir_node.body)
-        return ir_node
-    if isinstance(ir_node, ForGeneric):
-        ir_node.iterators = [remove_dead_code(i) for i in ir_node.iterators]
-        ir_node.body = remove_dead_code(ir_node.body)
-        return ir_node
-    if isinstance(ir_node, FunctionDef):
-        ir_node.body = remove_dead_code(ir_node.body)
-        return ir_node
-    if isinstance(ir_node, (Number, String, Boolean, Nil, Name, Vararg, Break)):
-        return ir_node
-    if isinstance(ir_node, BinOp):
-        ir_node.left = remove_dead_code(ir_node.left)
-        ir_node.right = remove_dead_code(ir_node.right)
-        return ir_node
-    if isinstance(ir_node, UnaryOp):
-        ir_node.operand = remove_dead_code(ir_node.operand)
-        return ir_node
-    if isinstance(ir_node, LocalDecl):
-        ir_node.values = [remove_dead_code(v) for v in ir_node.values]
-        return ir_node
-    if isinstance(ir_node, Assignment):
-        ir_node.targets = [remove_dead_code(t) for t in ir_node.targets]
-        ir_node.values = [remove_dead_code(v) for v in ir_node.values]
-        return ir_node
-    if isinstance(ir_node, Return):
-        ir_node.values = [remove_dead_code(v) for v in ir_node.values]
-        return ir_node
-    return ir_node
-
-def remove_unused_assignments(ir_node, used_vars=None):
-    if used_vars is None:
-        used_vars = set()
-    if isinstance(ir_node, Block):
-        new_stmts = []
-        for stmt in reversed(ir_node.statements):
-            if isinstance(stmt, LocalDecl):
-                if all(n.name not in used_vars and isinstance(v, (Number, String, Boolean, Nil)) for n, v in zip(stmt.names, stmt.values) if stmt.values):
-                    continue
-                new_stmts.append(stmt)
-                for n in stmt.names:
-                    used_vars.add(n.name)
-            elif isinstance(stmt, Assignment):
-                new_stmts.append(stmt)
-                for t in stmt.targets:
-                    if isinstance(t, Name):
-                        used_vars.add(t.name)
+    if isinstance(node, If):
+        if isinstance(node.test, Boolean):
+            if node.test.value is True:
+                return remove_dead_code(node.body)
             else:
-                new_stmts.append(stmt)
-        new_stmts.reverse()
-        return Block(new_stmts)
-    if isinstance(ir_node, If):
-        ir_node.body = remove_unused_assignments(ir_node.body, used_vars.copy())
-        if ir_node.orelse:
-            ir_node.orelse = remove_unused_assignments(ir_node.orelse, used_vars.copy())
-        return ir_node
-    if isinstance(ir_node, While):
-        ir_node.body = remove_unused_assignments(ir_node.body, used_vars.copy())
-        return ir_node
-    if isinstance(ir_node, Repeat):
-        ir_node.body = remove_unused_assignments(ir_node.body, used_vars.copy())
-        return ir_node
-    if isinstance(ir_node, ForNumeric):
-        ir_node.body = remove_unused_assignments(ir_node.body, used_vars.copy())
-        return ir_node
-    if isinstance(ir_node, ForGeneric):
-        ir_node.body = remove_unused_assignments(ir_node.body, used_vars.copy())
-        return ir_node
-    if isinstance(ir_node, FunctionDef):
-        ir_node.body = remove_unused_assignments(ir_node.body, used_vars.copy())
-        return ir_node
-    if isinstance(ir_node, LocalDecl):
-        for n in ir_node.names:
-            used_vars.add(n.name)
-        return ir_node
-    if isinstance(ir_node, Assignment):
-        for t in ir_node.targets:
-            if isinstance(t, Name):
-                used_vars.add(t.name)
-        return ir_node
-    return ir_node
+                return remove_dead_code(node.orelse) if node.orelse else None
+        n = copy.copy(node)
+        n.test   = remove_dead_code(node.test)
+        n.body   = remove_dead_code(node.body)
+        n.orelse = remove_dead_code(node.orelse) if node.orelse else None
+        return n
+    if isinstance(node, While):
+        if isinstance(node.test, Boolean) and node.test.value is False:
+            return None
+        n = copy.copy(node)
+        n.test = remove_dead_code(node.test)
+        n.body = remove_dead_code(node.body)
+        return n
+    if isinstance(node, (Repeat, ForNumeric, ForGeneric, FunctionDef)):
+        n = copy.copy(node)
+        n.body = remove_dead_code(node.body)
+        return n
+    if isinstance(node, (Number, String, Boolean, Nil, Name, Vararg, Break)):
+        return node
+    if isinstance(node, BinOp):
+        n = copy.copy(node)
+        n.left  = remove_dead_code(node.left)
+        n.right = remove_dead_code(node.right)
+        return n
+    if isinstance(node, UnaryOp):
+        n = copy.copy(node)
+        n.operand = remove_dead_code(node.operand)
+        return n
+    if isinstance(node, (LocalDecl, Assignment, Return)):
+        return node
+    return node
+
+def remove_unused_assignments(node):
+    if not isinstance(node, Block):
+        return node
+    used = set()
+    for stmt in node.statements:
+        if isinstance(stmt, LocalDecl):
+            for v in stmt.values:
+                _collect_used(v, used)
+        elif isinstance(stmt, Assignment):
+            for t in stmt.targets:
+                _collect_used(t, used)
+            for v in stmt.values:
+                _collect_used(v, used)
+        else:
+            _collect_used(stmt, used)
+    new_stmts = []
+    for stmt in node.statements:
+        if isinstance(stmt, LocalDecl):
+            all_unused = all(n.name not in used for n in stmt.names)
+            all_pure   = all(isinstance(v, (Number, String, Boolean, Nil))
+                             for v in stmt.values) if stmt.values else True
+            if all_unused and all_pure:
+                continue
+        new_stmts.append(stmt)
+    n = copy.copy(node)
+    n.statements = new_stmts
+    return n

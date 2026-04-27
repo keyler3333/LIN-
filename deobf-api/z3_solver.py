4bb484dda7db03aaea91fb52b27d7f2b43b8e449
keyler3333 <kyturk42@gmail.com>
2026-04-27 17:51:11 -0400

    Update z3_solver.py

--- deobf-api/z3_solver.py
+++ deobf-api/z3_solver.py
@@ -1,42 +1,25 @@
 import z3
-from expr import Const, Var, BinOp, Not, And
+from expr import Var, Const, BinOp
 
 def to_z3(e, env=None):
     env = env or {}
-
     if isinstance(e, Const):
-        return z3.BoolVal(e.value) if isinstance(e.value, bool) else z3.IntVal(e.value)
-
+        if isinstance(e.value, bool):
+            return z3.BoolVal(e.value)
+        return z3.IntVal(e.value)
     if isinstance(e, Var):
         if e.name not in env:
             env[e.name] = z3.Int(e.name)
         return env[e.name]
-
     if isinstance(e, BinOp):
         l = to_z3(e.left, env)
         r = to_z3(e.right, env)
-
-        if e.op == "+":
-            return l + r
-        if e.op == "-":
-            return l - r
-        if e.op == "*":
-            return l * r
-        if e.op == "/":
-            return l / r
-        if e.op == "==":
-            return l == r
-        if e.op == "<":
-            return l < r
-        if e.op == ">":
-            return l > r
-
-    if isinstance(e, Not):
-        return z3.Not(to_z3(e.value, env))
-
-    if isinstance(e, And):
-        return z3.And(to_z3(e.left, env), to_z3(e.right, env))
-
+        ops = {
+            '+': l + r, '-': l - r, '*': l * r, '/': l / r,
+            '==': l == r, '<': l < r, '>': l > r
+        }
+        if e.op in ops:
+            return ops[e.op]
     return None
 
 def solve(expr):

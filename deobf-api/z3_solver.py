2e5d645246eff92f23852d1f0ba7c18b0ef6db04
keyler3333 <kyturk42@gmail.com>
2026-04-27 17:18:00 -0400

    Update z3_solver.py

--- deobf-api/z3_solver.py
+++ deobf-api/z3_solver.py
@@ -1,31 +1,26 @@
 import z3
 
-def solve_condition(expr):
-    solver = z3.Solver()
-    solver.add(expr)
-    result = solver.check()
-    if result == z3.sat:
-        return True
-    elif result == z3.unsat:
-        return False
+def expr_to_z3(expr):
+    if expr is True:
+        return z3.BoolVal(True)
+    if expr is False:
+        return z3.BoolVal(False)
+
+    if isinstance(expr, tuple):
+        op = expr[0]
+
+        if op == "and":
+            return z3.And(expr_to_z3(expr[1]), expr_to_z3(expr[2]))
+        if op == "not":
+            return z3.Not(expr_to_z3(expr[1]))
+
     return None
 
-def expr_to_z3(expr):
-    from symbolic_exec import SymConst, SymVar, SymBinOp
-    if isinstance(expr, SymConst):
-        return z3.BoolVal(expr.value) if isinstance(expr.value, bool) else z3.RealVal(expr.value)
-    if isinstance(expr, SymVar):
-        return z3.Real(expr.name)
-    if isinstance(expr, SymBinOp):
-        left = expr_to_z3(expr.left)
-        right = expr_to_z3(expr.right)
-        op_map = {
-            '+': lambda a,b: a + b, '-': lambda a,b: a - b,
-            '*': lambda a,b: a * b, '/': lambda a,b: a / b,
-            '==': lambda a,b: a == b, '!=': lambda a,b: a != b,
-            '<': lambda a,b: a < b, '<=': lambda a,b: a <= b,
-            '>': lambda a,b: a > b, '>=': lambda a,b: a >= b
-        }
-        if expr.op in op_map:
-            return op_map[expr.op](left, right)
-    return None
+def solve_condition(expr):
+    s = z3.Solver()
+    z = expr_to_z3(expr)
+    if z is None:
+        return True
+
+    s.add(z)
+    return s.check() == z3.sat

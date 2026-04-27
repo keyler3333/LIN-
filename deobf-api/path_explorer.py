32487d00a85fdf3a78f3de68275a4a60aba9f7ac
keyler3333 <kyturk42@gmail.com>
2026-04-27 17:24:20 -0400

    Update path_explorer.py

--- deobf-api/path_explorer.py
+++ deobf-api/path_explorer.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from symbolic_engine import SymState, SymStore, execute_symbolic
+from symbolic_engine import SymState, SymStore, execute_symbolic, SSAInstruction
 from z3_solver import expr_to_z3, solve_condition
 
 class PathState:
@@ -10,48 +10,35 @@ class PathState:
 
 def _is_feasible(cond, path_cond):
     z3_cond = expr_to_z3(cond)
-    z3_path = expr_to_z3(path_cond)
-
+    z3_path = expr_to_z3(path_cond) if path_cond else None
     if z3_cond is None:
         return True
-
-    
-    return solve_condition(z3_path) if z3_path else True
+    if z3_path is not None:
+        return solve_condition(z3.Simplify(z3.And(z3_cond, z3_path)))
+    return solve_condition(z3_cond)
 
 def explore(entry_block):
     initial = PathState(entry_block, SymState(SymStore()), True)
     worklist = [initial]
     final_states = []
-
     while worklist:
         ps = worklist.pop()
         block = ps.block
-
-
         for instr in block.instructions:
             execute_symbolic(instr, ps.state)
-
-
         if block.condition and block.true_branch:
-
             cond = block.condition
             true_state = deepcopy(ps)
             false_state = deepcopy(ps)
-
             true_state.path_cond = ("and", ps.path_cond, cond)
             false_state.path_cond = ("and", ps.path_cond, ("not", cond))
-
             if _is_feasible(cond, ps.path_cond):
                 true_state.block = block.true_branch
                 worklist.append(true_state)
-
             if _is_feasible(("not", cond), ps.path_cond):
                 false_state.block = block.false_branch
                 worklist.append(false_state)
-
             continue
-
-
         if block.successors:
             for nxt in block.successors:
                 nxt_state = deepcopy(ps)
@@ -59,5 +46,4 @@ def explore(entry_block):
                 worklist.append(nxt_state)
         else:
             final_states.append(ps)
-
     return final_states

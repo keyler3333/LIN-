8ba8af8bda5763362521ab38424aae4f79328cee
keyler3333 <kyturk42@gmail.com>
2026-04-27 17:24:51 -0400

    Update z3_solver.py

--- deobf-api/z3_solver.py
+++ deobf-api/z3_solver.py
@@ -5,15 +5,12 @@ def expr_to_z3(expr):
         return z3.BoolVal(True)
     if expr is False:
         return z3.BoolVal(False)
-
     if isinstance(expr, tuple):
         op = expr[0]
-
         if op == "and":
             return z3.And(expr_to_z3(expr[1]), expr_to_z3(expr[2]))
         if op == "not":
             return z3.Not(expr_to_z3(expr[1]))
-
     return None
 
 def solve_condition(expr):
@@ -21,6 +18,5 @@ def solve_condition(expr):
     z = expr_to_z3(expr)
     if z is None:
         return True
-
     s.add(z)
     return s.check() == z3.sat
